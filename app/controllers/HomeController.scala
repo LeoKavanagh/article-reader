e78f72c06425355ecbb69bcf58ed9aa40090236f
Leo Kavanagh <leok90@gmail.com>
2018-12-27 19:14:22 +0000

    Redirect base url to simpleForm

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -13,7 +13,7 @@ import controllers.ArticleReader.process
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
   def index() = Action { 
-    implicit request: Request[AnyContent] => Ok(views.html.index())
+    implicit request: Request[AnyContent] => Redirect("/simpleForm")
   }
 
   def simpleForm() = Action { implicit request: Request[AnyContent] =>
